44c9ac65e5b0338345ea10bb0832e0d38be2d00c
unknown <aks152312@gmail.com>
2023-06-25 14:17:46 +0900

    file select 버전

--- main.py
+++ main.py
@@ -1,90 +1,147 @@
-import matplotlib.pyplot as plt
-import torch
+import os
 import cv2
 import math
-from torchvision import transforms
+import torch
 import numpy as np
-import os
-
 from tqdm import tqdm
-
+from torchvision import transforms
 from utils.datasets import letterbox
 from utils.general import non_max_suppression_kpt
-from utils.plots import output_to_keypoint, plot_skeleton_kpts
+from utils.plots import output_to_keypoint
+from PyQt5.QtWidgets import QApplication, QFileDialog, QMessageBox, QInputDialog
+from concurrent.futures import ThreadPoolExecutor
 
+RED_COLOR = (0, 0, 255)
+FONT = cv2.FONT_HERSHEY_SIMPLEX
+LINE = cv2.LINE_AA
+
+results_folder = 'results'
+
+'''
+    root에 results 폴더가 존재하지 않으면 알아서 생성
+'''
+if not os.path.exists(results_folder):
+    os.makedirs(results_folder)
+
+def select_folder():
+    folder_path = QFileDialog.getExistingDirectory(None, "Select Folder", options=QFileDialog.ShowDirsOnly)
+    return folder_path
+
+def select_file():
+    file_path, _ = QFileDialog.getOpenFileName(None, "Select File", filter="Video Files (*.mp4 *.avi *.wmv)")
+    return file_path
+
+def show_error_message(message):
+    msg_box = QMessageBox()
+    msg_box.setIcon(QMessageBox.Critical)
+    msg_box.setWindowTitle("Error")
+    msg_box.setText(message)
+    msg_box.exec_()
 
 def fall_detection(poses):
+    '''
+        지정한 낙상 조건에 부합하는지를 결정한다.
+        넘어진 것이 확인되었다면 True와 함께 좌표를 반환한다.
+    '''
     for pose in poses:
         xmin, ymin = (pose[2] - pose[4] / 2), (pose[3] - pose[5] / 2)
         xmax, ymax = (pose[2] + pose[4] / 2), (pose[3] + pose[5] / 2)
-        left_shoulder_y = pose[23]
         left_shoulder_x = pose[22]
+        left_shoulder_y = pose[23]
         right_shoulder_y = pose[26]
-        left_body_y = pose[41]
         left_body_x = pose[40]
+        left_body_y = pose[41]
         right_body_y = pose[44]
         len_factor = math.sqrt(((left_shoulder_y - left_body_y) ** 2 + (left_shoulder_x - left_body_x) ** 2))
         left_foot_y = pose[53]
         right_foot_y = pose[56]
+
         dx = int(xmax) - int(xmin)
         dy = int(ymax) - int(ymin)
         difference = dy - dx
-        if left_shoulder_y > left_foot_y - len_factor and left_body_y > left_foot_y - (
-                len_factor / 2) and left_shoulder_y > left_body_y - (len_factor / 2) or (
-                right_shoulder_y > right_foot_y - len_factor and right_body_y > right_foot_y - (
-                len_factor / 2) and right_shoulder_y > right_body_y - (len_factor / 2)) \
-                or difference < 0:
-            return True, (xmin, ymin, xmax, ymax)
-    return False, None
+
+        if (
+            left_shoulder_y > left_foot_y - len_factor and left_body_y > left_foot_y - (len_factor / 2) and
+            left_shoulder_y > left_body_y - (len_factor / 2)
+        ) or (
+            right_shoulder_y > right_foot_y - len_factor and right_body_y > right_foot_y - (len_factor / 2) and
+            right_shoulder_y > right_body_y - (len_factor / 2)
+        ) or difference < 0:
+            '''
+                어깨가 발보다 아래에 있고 몸이 발보다 아래에 있고 어깨가 몸보다 아래에 있거나
+                세로보다 가로가 더 긴 경우 (보통 사람은 서있을 때 세로가 더 길다)
+            '''
+            return True, (xmin, ymin, xmax, ymax) #넘어진 상태 (좌표와 함께 RETURN)
+    return False, None #넘어지지 않은 상태
 
 
-def falling_alarm(image, bbox):
+def draw_falling_alarm(image, bbox):
     x_min, y_min, x_max, y_max = bbox
-    cv2.rectangle(image, (int(x_min), int(y_min)), (int(x_max), int(y_max)), color=(0, 0, 255),
-                  thickness=5, lineType=cv2.LINE_AA)
-    cv2.putText(image, 'Person Fell down', (11, 100), 0, 1, [0, 0, 2550], thickness=3, lineType=cv2.LINE_AA)
+    cv2.rectangle(image, (int(x_min), int(y_min)), (int(x_max), int(y_max)), color=RED_COLOR,
+                  thickness=5, lineType=LINE)
+    
+    text = 'Person Fell down'
+    _, text_height = cv2.getTextSize(text, FONT, 1, 3)[0]
+    cv2.putText(image, text, (int(x_min), int(y_min - text_height - 10)), FONT, 1, RED_COLOR, thickness=3, lineType=LINE)
 
 
 def get_pose_model():
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     print("device: ", device)
     weigths = torch.load('yolov7-w6-pose.pt', map_location=device)
-    model = weigths['model']
-    _ = model.float().eval()
+    model = weigths['model'].float().eval()
     if torch.cuda.is_available():
         model = model.half().to(device)
     return model, device
 
 
 def get_pose(image, model, device):
-    image = letterbox(image, 960, stride=64, auto=True)[0]
+    image = letterbox(image, 960, stride=128, auto=True)[0]
     image = transforms.ToTensor()(image)
-    image = torch.tensor(np.array([image.numpy()]))
+    image = image.unsqueeze(0)
+
     if torch.cuda.is_available():
         image = image.half().to(device)
+
     with torch.no_grad():
         output, _ = model(image)
+
     output = non_max_suppression_kpt(output, 0.25, 0.65, nc=model.yaml['nc'], nkpt=model.yaml['nkpt'],
                                      kpt_label=True)
+    
     with torch.no_grad():
         output = output_to_keypoint(output)
     return image, output
 
 
 def prepare_image(image):
+    _image = image[0].permute(1, 2, 0) * 255 # BGR to RGB
     _image = image[0].permute(1, 2, 0) * 255
+		# 이미지를 CPU 상에서 NumPy 배열로 변환
     _image = _image.cpu().numpy().astype(np.uint8)
+		# RGB 형식의 이미지를 BGR 형식으로 변환
     _image = cv2.cvtColor(_image, cv2.COLOR_RGB2BGR)
     return _image
 
 
 def prepare_vid_out(video_path, vid_cap):
-    vid_write_image = letterbox(vid_cap.read()[1], 960, stride=64, auto=True)[0]
+    vid_write_image = letterbox(vid_cap.read()[1], 960, stride=128, auto=True)[0]
     resize_height, resize_width = vid_write_image.shape[:2]
-    out_video_name = f"{video_path.split('/')[-1].split('.')[0]}_keypoint.mp4"
-    out = cv2.VideoWriter(out_video_name, cv2.VideoWriter_fourcc(*'mp4v'), 30, (resize_width, resize_height))
+    '''
+        video 가져오는 방법 수정 필요
+    '''
+    out_video_name = f"{os.path.splitext(os.path.basename(video_path))[0]}_keypoint.mp4"
+    out_video_path = os.path.join(results_folder, out_video_name)
+    out = cv2.VideoWriter(out_video_path, cv2.VideoWriter_fourcc(*'mp4v'), 30, (resize_width, resize_height))
     return out
 
+def process_frame(frame, model, device):
+    image, output = get_pose(frame, model, device)
+    _image = prepare_image(image)
+    is_fall, bbox = fall_detection(output)
+    if is_fall:
+        draw_falling_alarm(_image, bbox)
+    return _image
 
 def process_video(video_path):
     vid_cap = cv2.VideoCapture(video_path)
@@ -96,26 +153,50 @@ def process_video(video_path):
     model, device = get_pose_model()
     vid_out = prepare_vid_out(video_path, vid_cap)
 
+    frames = []
     success, frame = vid_cap.read()
-    _frames = []
+
     while success:
-        _frames.append(frame)
+        frames.append(frame)
         success, frame = vid_cap.read()
 
-    for image in tqdm(_frames):
-        image, output = get_pose(image, model, device)
-        _image = prepare_image(image)
-        is_fall, bbox = fall_detection(output)
-        if is_fall:
-            falling_alarm(_image, bbox)
+    for frame in frames:
+        _image = process_frame(frame, model, device)
         vid_out.write(_image)
 
     vid_out.release()
-    vid_cap.release()
+    print("Video processing complete:", video_path)
+
+def main():
+    app = QApplication([])
+    options = ("Select Folder", "Select Video File")
+    option, _ = QInputDialog.getItem(None, "Options", "Select an option:", options, 0, False)
+
+    if option == "Select Folder":
+        selected_path = select_folder()
+        if selected_path:
+            video_files = []
+            for file in os.listdir(selected_path):
+                if file.endswith(('.mp4', '.avi', '.wmv')):
+                    video_path = os.path.join(selected_path, file)
+                    video_files.append(video_path)
+
+            if video_files:
+                for video_path in video_files:
+                    process_video(video_path)
+            else:
+                print("No video files found in the selected folder.")
+        else:
+            print("Invalid folder selection.")
+    elif option == "Select Video File":
+        video_path = QFileDialog.getOpenFileName(None, "Select Video File", "", "Video Files (*.mp4 *.avi *.wmv)")[0]
+        if video_path:
+            process_video(video_path)
+        else:
+            print("Invalid video file selection.")
+
+    app.quit()
 
 
 if __name__ == '__main__':
-    videos_path = 'fall_dataset/videos'
-    for video in os.listdir(videos_path):
-        video_path = os.path.join(videos_path, video)
-        process_video(video_path)
+    main()
